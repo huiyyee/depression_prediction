5c00a97dd788db9933c04d7cb32970f1725ba8dc
huiyyee <95178914+huiyyee@users.noreply.github.com>
2024-07-14 20:05:51 +0800

    Update testing_app-2.py

--- testing_app-2.py
+++ testing_app-2.py
@@ -31,11 +31,6 @@ with open(os.path.join(save_path, 'best_algo.pkl'), 'wb') as f:
     pickle.dump(best_algo, f)
 """
 
-pip install nltk
-pip install streamlit
-pip install contractions
-pip install biopython
-
 import streamlit as st
 import pickle
 import re
